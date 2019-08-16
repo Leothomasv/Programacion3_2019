3077a7d8c27ab3534298ccb4c89c9edb6e278946
Leothomasv <leothomasv@unitec.edu>
2019-08-16 11:35:18 -0600

    Examen

---
+++ Examen1/Cadena.cpp
@@ -0,0 +1,31 @@
+#include "Cadena.h"
+#include <iostream>
+
+
+Cadena::Cadena(char * c1) {
+	cadena = new char[strlen(c1)]; //RESERVANDO ESPACIO EN MEMORIA
+	strcpy(cadena, c1); // copiar los bytes 
+}
+
+Cadena::Cadena(Cadena &x) {
+	this->cadena = x.cadena;
+	this->SizeCadena = x.SizeCadena;
+
+}
+
+Cadena::Cadena(void) {
+	cadena = new char[1];
+	strcpy(cadena, " ");
+}
+
+Cadena operator +(Cadena&x, Cadena&y) {
+
+}
+
+ostream& operator << (ostream&x, const Cadena &y) {
+	x << y.cadena;
+	return x;
+}
+
+
+
